79e5068931542fb2980d96b3cb72de6fd0f07d7a
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-12 22:54:33 +0300

    0201-sign-up-form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'signup',
+    component: SignUpPageComponent
+}];
