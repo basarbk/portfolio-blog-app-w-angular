8abe326e2f30d5d0052d912e250a7bf4077360ff
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-12 23:05:48 +0300

    0202-interactions

--- src/app/pages/sign-up-page/sign-up-page.component.ts
+++ src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up-page.component.html'
 })
 export class SignUpPageComponent {
 
+  email = new FormControl<string>('')
+
+  isDisabled(){
+    return !this.email.value
+  }
 }
