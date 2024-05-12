98579f32c17817e443ccf7ec25f0554e18b5fe35
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-12 23:35:44 +0300

    0203-submit

--- src/app/pages/sign-up-page/sign-up-page.component.ts
+++ src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -8,10 +9,16 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './sign-up-page.component.html'
 })
 export class SignUpPageComponent {
+  private apiService = inject(ApiService)
 
-  email = new FormControl<string>('')
+  email = new FormControl<string>('', {nonNullable: true})
 
   isDisabled(){
     return !this.email.value
+  }
+
+  submit(event: Event){
+    event.preventDefault()
+    this.apiService.signUp(this.email.value).subscribe()
   }
 }
