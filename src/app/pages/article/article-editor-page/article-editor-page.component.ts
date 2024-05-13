52782c0507ffc9f606175104c8ce16b6ed744078
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-14 00:57:51 +0300

    0506-validation

--- src/app/pages/article/article-editor-page/article-editor-page.component.ts
+++ src/app/pages/article/article-editor-page/article-editor-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ArticleService } from '../../../shared/article.service';
 import { PublishButtonComponent } from '../components/publish-button/publish-button.component';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-article-editor-page',
@@ -13,13 +14,59 @@ import { PublishButtonComponent } from '../components/publish-button/publish-but
 export class ArticleEditorPageComponent {
   private articleService = inject(ArticleService);
 
-  title = new FormControl<string>('', { nonNullable: true });
-  content = new FormControl<string>('', { nonNullable: true });
+  title = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+  content = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
 
   id: number = 0;
   apiProgress = false;
 
+  errors:
+    | { title: string | undefined; content: string | undefined }
+    | undefined;
+
+  constructor() {
+    this.title.valueChanges.subscribe(() => {
+      if (this.errors?.title) {
+        this.errors.title = undefined;
+      }
+    });
+    this.content.valueChanges.subscribe(() => {
+      if (this.errors?.content) {
+        this.errors.content = undefined;
+      }
+    });
+  }
+
+  checkValidity() {
+    let valid = true;
+    const validationErrors: typeof this.errors = {
+      title: undefined,
+      content: undefined,
+    };
+
+    if (this.title.errors) {
+      validationErrors.title = 'Invalid title';
+      valid = false;
+    }
+
+    if (this.content.errors) {
+      validationErrors.content = 'Invalid content';
+      valid = false;
+    }
+
+    this.errors = validationErrors;
+
+    return valid;
+  }
+
   submit() {
+    if (!this.checkValidity()) return;
     this.apiProgress = true;
     this.articleService
       .createOrUpdateArticle(
@@ -29,9 +76,17 @@ export class ArticleEditorPageComponent {
         },
         this.id
       )
-      .subscribe((data) => {
-        this.apiProgress = false;
-        this.id = data.id;
+      .subscribe({
+        next: (data) => {
+          this.apiProgress = false;
+          this.id = data.id;
+        },
+        error: (httpError: HttpErrorResponse) => {
+          this.apiProgress = false;
+          if (httpError.status === 400) {
+            this.errors = httpError.error.validationErrors;
+          }
+        },
       });
   }
 }
