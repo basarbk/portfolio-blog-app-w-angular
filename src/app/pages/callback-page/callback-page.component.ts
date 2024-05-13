b3c74253471e1f58de67fd0b3a6773893f087c90
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-13 16:12:29 +0300

    0404-progress-n-response

--- src/app/pages/callback-page/callback-page.component.ts
+++ src/app/pages/callback-page/callback-page.component.ts
@@ -1,11 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AlertComponent } from '../../components/alert/alert.component';
 
 @Component({
   selector: 'app-callback-page',
   standalone: true,
-  imports: [],
+  imports: [AlertComponent],
   templateUrl: './callback-page.component.html'
 })
 export class CallbackPageComponent implements OnInit{
@@ -13,13 +14,31 @@ export class CallbackPageComponent implements OnInit{
   private route = inject(ActivatedRoute);
 
   private httpClient = inject(HttpClient);
+
+  status: 'loading' | 'success' | 'fail' = 'loading'
+
+  message = '';
   
   ngOnInit(): void {
     this.route.queryParams.subscribe((queryParams) => {
+      this.status = 'loading'
       this.httpClient.post('/api/auth', {
         token: queryParams['token'],
         operation: queryParams['operation']
-      }).subscribe()
+      }).subscribe({
+        next: () => {
+          this.status = 'success'
+          this.message = 'Account is created'
+        },
+        error: (httpError: HttpErrorResponse) => {
+          this.status = 'fail'
+          if(httpError.status) {
+            this.message = httpError.error.message
+          } else {
+            this.message = 'Unexpected error occurred, please try again'
+          }
+        }
+      })
     })
     
   }
