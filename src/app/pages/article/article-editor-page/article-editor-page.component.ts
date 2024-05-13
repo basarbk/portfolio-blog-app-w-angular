70185ad8e78d376db66e524457921d8b2445b8c1
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-13 23:52:47 +0300

    0502-editor-layout

--- src/app/pages/article/article-editor-page/article-editor-page.component.ts
+++ src/app/pages/article/article-editor-page/article-editor-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../components/button/button.component';
 
 @Component({
   selector: 'app-article-editor-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './article-editor-page.component.html'
 })
 export class ArticleEditorPageComponent {
