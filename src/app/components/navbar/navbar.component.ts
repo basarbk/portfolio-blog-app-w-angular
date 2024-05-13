eb12a70aa5b24a009cb498b5097eb5f21ad5b000
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-13 15:50:33 +0300

    0402-navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+
+}
