60fae3a111486b5e6cb6decd37414eb49531d452
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-13 15:37:21 +0300

    0401-new-pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CallbackPageComponent } from './pages/callback-page/callback-page.component';
 
 export const routes: Routes = [{
     path: 'signup',
     component: SignUpPageComponent
-}];
+}, {
+    path: '',
+    component: HomePageComponent
+},
+{
+    path: 'callback',
+    component: CallbackPageComponent,
+}
+
+];
---
+++ src/app/pages/callback-page/callback-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-callback-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './callback-page.component.html'
+})
+export class CallbackPageComponent {
+
+}
