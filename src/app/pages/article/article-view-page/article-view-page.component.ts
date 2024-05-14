bfcaee88f1c44ad0617a8cc297b30617b2af49d9
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-14 15:09:01 +0300

    0603-progress-n-error

--- src/app/pages/article/article-view-page/article-view-page.component.ts
+++ src/app/pages/article/article-view-page/article-view-page.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ArticleService } from '../../../shared/article.service';
 import { ActivatedRoute } from '@angular/router';
 import { Article } from '../../../shared/types';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SpinnerComponent } from '../../../components/spinner/spinner.component';
+import { AlertComponent } from '../../../components/alert/alert.component';
 
 @Component({
   selector: 'app-article-view-page',
   standalone: true,
-  imports: [],
+  imports: [SpinnerComponent, AlertComponent],
   templateUrl: './article-view-page.component.html',
 })
 export class ArticleViewPageComponent implements OnInit {
@@ -15,10 +18,25 @@ export class ArticleViewPageComponent implements OnInit {
 
   article: Article | undefined;
 
+  status: 'loading' | 'success' | 'fail' = 'loading';
+  message: string = '';
+
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      this.articleService.fetchArticle(params['idOrSlug']).subscribe((data) => {
-        this.article = data;
+      this.status = 'loading';
+      this.articleService.fetchArticle(params['idOrSlug']).subscribe({
+        next: (data) => {
+          this.status = 'success';
+          this.article = data;
+        },
+        error: (httpError: HttpErrorResponse) => {
+          this.status = 'fail';
+          if (httpError.status) {
+            this.message = httpError.error.message;
+          } else {
+            this.message = 'Unexpected error occurred, please try again';
+          }
+        },
       });
     });
   }
