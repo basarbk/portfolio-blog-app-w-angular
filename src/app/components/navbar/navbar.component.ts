8d14824228b0fb2d2db81de9c9afe70d0de5e04b
Basar Buyukkahraman <basarbk@yahoo.com>
2024-05-16 15:36:46 +0300

    0803-user-on-navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../shared/auth.service';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
-  templateUrl: './navbar.component.html'
+  imports: [RouterLink, UserComponent],
+  templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-
-  loggedIn = false
+  loggedIn = false;
 
   constructor(private authService: AuthService) {
     this.authService.user.subscribe((data) => {
-      this.loggedIn = data.id > 0
-    })
+      this.loggedIn = data.id > 0;
+    });
   }
 
-  logout(){
-    this.authService.logout().subscribe()
+  logout() {
+    this.authService.logout().subscribe();
   }
-
 }
---
+++ src/app/components/navbar/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { ImageWithFallbackComponent } from '../../image-with-fallback/image-with-fallback.component';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../shared/auth.service';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [ImageWithFallbackComponent, RouterLink],
+  templateUrl: './user.component.html',
+})
+export class UserComponent {
+  private authService = inject(AuthService);
+
+  get image() {
+    return this.authService.user.getValue().image;
+  }
+
+  get name() {
+    return this.authService.user.getValue().name;
+  }
+
+  get link() {
+    return `/${this.authService.user.getValue().handle}`;
+  }
+}
